d60e9dddfe431f75ac98e29857e36cfad664fdc3
Usuario <Usuario@IVAN-PC>
2021-04-18 17:14:37 +0200

    Segundo commit cambios(cambios2)

---
+++ actividad4/src/cambios/cambios2.java
@@ -0,0 +1,10 @@
+package cambios;
+
+public class cambios2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Pruebas Git. Se añade cambio2.1.");
+	}
+
+}
